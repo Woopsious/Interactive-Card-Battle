8acd04801694e7504f0fb240eff3b65b9efabc3e
Woopsious <reecehand@hotmail.co.uk>
2025-07-10 06:19:37 +0100

    spent 45mins reworking logic to remove duplicate attack data moves showing up in inspector + renaming attack data names

--- Assets/Scripts/!Editor/EntityDataEditor.cs
+++ Assets/Scripts/!Editor/EntityDataEditor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -13,14 +14,14 @@ namespace Woopsious
 		EntityData data;
 
 		//move set attack data
-		List<List<SerializedProperty>> attackDataList = new();
-		List<List<Editor>> attackDataEditorList = new();
-		List<List<bool>> showAttackDataList = new();
+		List<SerializedProperty> attackDataList = new();
+		List<Editor> attackDataEditorList = new();
+		List<bool> showAttackDataList = new();
 
 		void OnEnable()
 		{
 			data = (EntityData)target;
-			BuildNestedLists();
+			BuildLists();
 		}
 
 		public override void OnInspectorGUI()
@@ -49,7 +50,7 @@ namespace Woopsious
 			{
 				EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(data.moveSetOrder)), true);
 
-				BuildMoveSetEditorUi();
+				BuildAttackEditorUi();
 			}
 
 			// Write back changed values
@@ -57,7 +58,7 @@ namespace Woopsious
 			serializedObject.ApplyModifiedProperties();
 		}
 
-		void BuildNestedLists()
+		void BuildLists()
 		{
 			attackDataList.Clear();
 			attackDataEditorList.Clear();
@@ -65,62 +66,50 @@ namespace Woopsious
 
 			SerializedProperty moveSetOrderProp = serializedObject.FindProperty(nameof(data.moveSetOrder));
 
-			for (int i = 0; i < moveSetOrderProp.arraySize; i++) //build nested lists
+			for (int i = 0; i < data.moveSetOrder.Count; i++)
 			{
 				SerializedProperty moveSetDataProp = moveSetOrderProp.GetArrayElementAtIndex(i);
 				SerializedProperty moveSetMovesProp = moveSetDataProp.FindPropertyRelative("moveSetMoves");
 
-				List<SerializedProperty> nestedAttackDataList = new();
-				List<Editor> nestedAttackDataEditorList = new();
-				List<bool> nestedShowAttackDataList = new();
-
 				for (int j = 0; j < moveSetMovesProp.arraySize; j++) //populate nested lists
 				{
-					nestedAttackDataList.Add(moveSetMovesProp.GetArrayElementAtIndex(j));
-					nestedAttackDataEditorList.Add(null);
-					nestedShowAttackDataList.Add(false);
+					Object newObjRef = moveSetMovesProp.GetArrayElementAtIndex(j).objectReferenceValue;
+					if (attackDataList.Any(p => p.objectReferenceValue == newObjRef)) continue;
+
+					attackDataList.Add(moveSetMovesProp.GetArrayElementAtIndex(j));
+					attackDataEditorList.Add(null);
+					showAttackDataList.Add(false);
+				}
+			}
+		}
+
+		void BuildAttackEditorUi()
+		{
+			for (int i = 0; i < attackDataList.Count; i++)
+			{
+				if (attackDataList[i].objectReferenceValue == null) return;
+
+				if (attackDataEditorList[i] == null || attackDataEditorList[i].target != attackDataList[i].objectReferenceValue)
+					attackDataEditorList[i] = CreateEditor(attackDataList[i].objectReferenceValue);
+
+				//grab attack name to set as fold out label
+				AttackData data = attackDataList[i].objectReferenceValue as AttackData;
+				string labelName = !string.IsNullOrEmpty(data.attackName) ? data.attackName + " " : "";
+				showAttackDataList[i] = EditorGUILayout.Foldout(showAttackDataList[i], labelName + "Attack Data", true);
+
+				//add indented box
+				EditorGUILayout.BeginVertical("box");
+				EditorGUI.indentLevel++;
+
+				if (showAttackDataList[i])
+				{
+					EditorGUILayout.LabelField(labelName + "Attack Data", EditorStyles.boldLabel);
+					attackDataEditorList[i].OnInspectorGUI();
 				}
 
-				attackDataList.Add(nestedAttackDataList);
-				attackDataEditorList.Add(nestedAttackDataEditorList);
-				showAttackDataList.Add(nestedShowAttackDataList);
+				EditorGUI.indentLevel--;
+				EditorGUILayout.EndVertical();
 			}
-		}
-
-		void BuildMoveSetEditorUi()
-		{
-			for (int i = 0; i < data.moveSetOrder.Count; i++) //loop through move sets
-			{
-				MoveSetData moveSetData = data.moveSetOrder[i];
-
-				for (int j = 0; j < moveSetData.moveSetMoves.Count; j++) //loop through moves in move set to build ui
-					BuildAttackDataUi(i, j);
-			}
-		}
-		void BuildAttackDataUi(int i, int j)
-		{
-			if (attackDataList[i][j].objectReferenceValue == null) return;
-
-			if (attackDataEditorList[i][j] == null || attackDataEditorList[i][j].target != attackDataList[i][j].objectReferenceValue)
-				attackDataEditorList[i][j] = CreateEditor(attackDataList[i][j].objectReferenceValue);
-
-			//grab attack name to set as fold out label
-			AttackData data = attackDataList[i][j].objectReferenceValue as AttackData;
-			string labelName = !string.IsNullOrEmpty(data.attackName) ? data.attackName + " " : "";
-			showAttackDataList[i][j] = EditorGUILayout.Foldout(showAttackDataList[i][j], labelName + "Attack Data", true);
-
-			//add indented box
-			EditorGUILayout.BeginVertical("box");
-			EditorGUI.indentLevel++;
-
-			if (showAttackDataList[i][j])
-			{
-				EditorGUILayout.LabelField(labelName + "Attack Data", EditorStyles.boldLabel);
-				attackDataEditorList[i][j].OnInspectorGUI();
-			}
-
-			EditorGUI.indentLevel--;
-			EditorGUILayout.EndVertical();
 		}
 	}
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 namespace Woopsious
 {
